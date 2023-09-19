059420694fa2fa1591488d5fe810d76ab799d05e
Eric Ma <ericmajinglong@gmail.com>
2023-09-19 07:44:35 -0400

    feat(tests): add test cases for matching function
    - Add test cases to verify the functionality of the matching function in the test_matching.py file.
    - The test cases cover different scenarios, including matching and non-matching genotype states at various positions.
    - The expected results are also included for each test case.
    
    [END COMMIT MESSAGE]

--- tests/test_matching.py
+++ tests/test_matching.py
@@ -10,45 +10,45 @@ from protein_reference_free_analysis.matching import (
 test_data_match_two = [
     # Test 1: Matching two genotype states at positions 0 and 1.
     (
-        np.array([0, 1]),
-        np.array([[0, 1, 0], [1, 0, 0]]),
-        np.array([[0, 1, 0], [1, 0, 0], [0, 1, 0], [0, 0, 1]]),
-        True,
+        np.array([0, 1]),  # sites to check for match
+        np.array([[0, 1, 0], [1, 0, 0]]),  # states to check
+        np.array([[0, 1, 0], [1, 0, 0], [0, 1, 0], [0, 0, 1]]),  # genotype to check
+        True,  # expected result
     ),
     # Test 2: Not matching two genotype states at positions 0 and 1.
     (
-        np.array([0, 1]),
-        np.array([[0, 1, 0], [1, 0, 0]]),
-        np.array([[1, 0, 0], [1, 0, 0], [0, 1, 0], [0, 0, 1]]),
-        False,
+        np.array([0, 1]),  # sites to check for match
+        np.array([[0, 1, 0], [1, 0, 0]]),  # states to check
+        np.array([[1, 0, 0], [1, 0, 0], [0, 1, 0], [0, 0, 1]]),  # genotype to check
+        False,  # expected result
     ),
     # Test 3: Matching two different genotype states at positions 2 and 0.
     (
-        np.array([2, 0]),
-        np.array([[0, 0, 1], [0, 1, 0]]),
-        np.array([[0, 1, 0], [1, 0, 0], [0, 0, 1], [1, 0, 0]]),
-        True,
+        np.array([2, 0]),  # sites to check for match
+        np.array([[0, 0, 1], [0, 1, 0]]),  # states to check
+        np.array([[0, 1, 0], [1, 0, 0], [0, 0, 1], [1, 0, 0]]),  # genotype to check
+        True,  # expected result
     ),
     # Test 4: Matching two genotype states when one of the sites is out of range.
     (
-        np.array([0, 1]),
-        np.array([[0, 1, 0], [1, 0, 0]]),
-        np.array([[0, 1, 0], [1, 0, 0], [0, 0, 1], [1, 0, 0]]),
-        True,
+        np.array([0, 1]),  # sites to check for match
+        np.array([[0, 1, 0], [1, 0, 0]]),  # states to check
+        np.array([[0, 1, 0], [1, 0, 0], [0, 0, 1], [1, 0, 0]]),  # genotype to check
+        True,  # expected result
     ),
     # Test 5: Matching one genotype state at position 0.
     (
-        np.array([0]),
-        np.array([[0, 1, 0]]),
-        np.array([[0, 1, 0], [1, 0, 0], [0, 1, 0], [0, 0, 0]]),
-        True,
+        np.array([0]),  # sites to check for match
+        np.array([[0, 1, 0]]),  # states to check
+        np.array([[0, 1, 0], [1, 0, 0], [0, 1, 0], [0, 0, 0]]),  # genotype to check
+        True,  # expected result
     ),
     # Test 6: Not matching one genotype state at position 0.
     (
-        np.array([0]),
-        np.array([[0, 1, 0]]),
-        np.array([[1, 0, 0], [1, 0, 0], [0, 1, 0], [0, 0, 1]]),
-        False,
+        np.array([0]),  # sites to check for match
+        np.array([[0, 1, 0]]),  # states to check
+        np.array([[1, 0, 0], [1, 0, 0], [0, 1, 0], [0, 0, 1]]),  # genotype to check
+        False,  # expected result
     ),
 ]
 
