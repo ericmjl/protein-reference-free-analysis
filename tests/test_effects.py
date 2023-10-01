a75fa06ea16816f0ba0380f9ed125a98dd314864
Eric Ma <ericmajinglong@gmail.com>
2023-09-30 23:39:49 -0400

    feat(tests): add second order effects test
    - Added a new test for second order effects in the test_effects.py file.
    - The test checks the shape of the second-order effects and verifies that the sum of the effects is close to zero.
    - Also added an additional assertion in the first order effects test to check the sum of the effects.

--- tests/test_effects.py
+++ tests/test_effects.py
@@ -6,6 +6,7 @@ from jax import random
 from protein_reference_free_analysis.effects import (
     calculate_single_genotype_averages,
     first_order_effects,
+    second_order_effects,
     zeroth_order_effects,
 )
 from protein_reference_free_analysis.genotype_generator import (
@@ -69,3 +70,20 @@ def test_first_order_effects(genotypes, seed):
     expected_shape = (num_sites, num_states)
     first_order_fx = first_order_effects(genotypes, phenotypes)
     assert first_order_fx.shape == expected_shape
+    assert np.isclose(first_order_fx.sum(), 0, atol=1e-6)
+
+
+@pytest.mark.parametrize("seed", [0, 10, 20, 30])
+def test_second_order_effects(genotypes, seed):
+    """Test that the shape of the second-order effects is correct.
+
+    :param genotypes: The genotypes to test. Comes from the genotypes() fixture.
+    :param seed: The seed to use.
+    """
+    _, num_sites, num_states = genotypes.shape
+    key = random.PRNGKey(seed)
+    phenotypes = random.normal(key, (num_states**num_sites,))
+    expected_shape = (num_sites, num_states, num_sites, num_states)
+    second_order_fx = second_order_effects(genotypes, phenotypes)
+    assert second_order_fx.shape == expected_shape
+    assert np.isclose(second_order_fx.sum(), 0, atol=1e-6)
