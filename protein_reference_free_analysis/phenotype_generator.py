58722e025da80861fcb93b718d86c548007e80c8
Eric Ma <ericmajinglong@gmail.com>
2023-10-01 10:46:00 -0400

    feat: Enhance phenotype_generator with random phenotype generation
    This commit introduces a new function `random_phenotype` in `phenotype_generator.py`
    that generates a random phenotype drawn from a Gaussian.
    The function `count_kth_genotype` is also updated to include type hints for better code readability and understanding.
    
    In addition, the test suite `test_overall_model.py` is updated to include tests
    for the new `random_phenotype` function and the updated `count_kth_genotype` function.
    
    This enhancement provides more flexibility in phenotype generation and improves the robustness of the test suite.

--- protein_reference_free_analysis/phenotype_generator.py
+++ protein_reference_free_analysis/phenotype_generator.py
@@ -1,12 +1,45 @@
 """Phenotype generators from genotype data."""
 import jax.numpy as np
+from jax import random
 
 
-def count_kth_genotype(genotype, k=0):
+def count_kth_genotype(genotype: np.ndarray, k=0):
     """Count the number of kth genotypes.
+
+    Usage example:
+
+    ```python
+    from jax import vmap
+    from functools import partial
+
+    genotypes = ...
+    k = 1
+    phenotypes = vmap(partial(count_kth_genotype, k=k))(genotypes)
+    ```
 
     :param genotype: The genotype to count.
     :param k: The kth genotype.
     :return: The number of kth genotypes.
     """
     return np.sum(genotype, axis=0)[k]
+
+
+def random_phenotype(genotype: np.ndarray, key: random.PRNGKey) -> np.ndarray:
+    """Generate a random phenotype drawn from a Gaussian.
+
+    Usage example:
+
+    ```python
+    from jax import vmap
+
+    genotypes = ...
+    key = random.PRNGKey(22)
+    keys = random.split(key, len(genotypes))
+    phenotypes = vmap(random_phenotype)(genotypes, keys)
+    ```
+
+    :param genotype: The genotype.
+    :param key: The key to use for random number generation.
+    :returns: A random phenotype.
+    """
+    return random.normal(key)
--- tests/test_overall_model.py
+++ tests/test_overall_model.py
@@ -3,7 +3,7 @@ from functools import partial
 
 import pytest
 from jax import numpy as np
-from jax import vmap
+from jax import random, vmap
 
 from protein_reference_free_analysis.effects import (
     first_order_effects,
@@ -15,7 +15,10 @@ from protein_reference_free_analysis.effects import (
 from protein_reference_free_analysis.genotype_generator import (
     make_comprehensive_genotypes,
 )
-from protein_reference_free_analysis.phenotype_generator import count_kth_genotype
+from protein_reference_free_analysis.phenotype_generator import (
+    count_kth_genotype,
+    random_phenotype,
+)
 
 
 @pytest.mark.parametrize("k", [0, 1, 2])
@@ -34,6 +37,37 @@ def test_overall_model_count_kth_genotype(k, num_states, num_sites):
     genotypes = make_comprehensive_genotypes(num_states=num_states, num_sites=num_sites)
     count_geno = partial(count_kth_genotype, k=k)
     phenotypes = vmap(count_geno)(genotypes)
+    e_0 = zeroth_order_effects(genotypes, phenotypes)
+    e_1 = first_order_effects(genotypes, phenotypes)
+    e_2 = second_order_effects(genotypes, phenotypes)
+
+    for idx, genotype in enumerate(genotypes):
+        recalculated_phenotype = (
+            e_0
+            + get_first_order_effect(e_1, genotype)
+            + get_second_order_effect(e_2, genotype)
+        )
+        assert np.allclose(recalculated_phenotype, phenotypes[idx], atol=1e-5)
+
+
+@pytest.mark.parametrize("seed", [0, 10, 20])
+@pytest.mark.parametrize("num_states", [3, 4])
+@pytest.mark.parametrize("num_sites", [2])  # only works with 2 sites, but not any more
+def test_overall_model_random_genotype(seed, num_states, num_sites):
+    """Test that the overall model works as expected with random genotypes.
+
+    This test tests that after calculating zeroth, first, and second order effects,
+    they can be recalculated correctly.
+
+    :param seed: Seed number for JAX's PRNG.
+    :param num_states: The number of states per site.
+    :param num_sites: The number of sites.
+    """
+    genotypes = make_comprehensive_genotypes(num_states=num_states, num_sites=num_sites)
+
+    key = random.PRNGKey(seed)
+    keys = random.split(key, len(genotypes))
+    phenotypes = vmap(random_phenotype)(genotypes, keys)
     e_0 = zeroth_order_effects(genotypes, phenotypes)
     e_1 = first_order_effects(genotypes, phenotypes)
     e_2 = second_order_effects(genotypes, phenotypes)
