d1272ca2ef3d07a6acd548feeea756ebb914a096
Eric Ma <ericmajinglong@gmail.com>
2023-10-01 10:12:46 -0400

    feat(tests): Add integration test for overall model
    This commit introduces a new integration test for the overall model.
    The test checks the calculation of zeroth, first, and second order effects and their recalculation.
    It uses parameterized inputs for the k-th genotype index, number of states per site, and number of sites.
    The test ensures that the recalculated phenotype matches the original phenotype within a tolerance level.

---
+++ tests/test_overall_model.py
@@ -0,0 +1,47 @@
+"""Integration test for the overall model."""
+from functools import partial
+
+import pytest
+from jax import numpy as np
+from jax import vmap
+
+from protein_reference_free_analysis.effects import (
+    first_order_effects,
+    get_first_order_effect,
+    get_second_order_effect,
+    second_order_effects,
+    zeroth_order_effects,
+)
+from protein_reference_free_analysis.genotype_generator import (
+    make_comprehensive_genotypes,
+)
+from protein_reference_free_analysis.phenotype_generator import count_kth_genotype
+
+
+@pytest.mark.parametrize("k", [0, 1, 2])
+@pytest.mark.parametrize("num_states", [3, 4])
+@pytest.mark.parametrize("num_sites", [2, 3])
+def test_overall_model_count_kth_genotype(k, num_states, num_sites):
+    """Test that the overall model works as expected.
+
+    This test tests that after calculating zeroth, first, and second order effects,
+    they can be recalculated correctly.
+
+    :param k: The index of the k-th genotype to calculate.
+    :param num_states: The number of states per site.
+    :param num_sites: The number of sites.
+    """
+    genotypes = make_comprehensive_genotypes(num_states=num_states, num_sites=num_sites)
+    count_geno = partial(count_kth_genotype, k=k)
+    phenotypes = vmap(count_geno)(genotypes)
+    e_0 = zeroth_order_effects(genotypes, phenotypes)
+    e_1 = first_order_effects(genotypes, phenotypes)
+    e_2 = second_order_effects(genotypes, phenotypes)
+
+    for idx, genotype in enumerate(genotypes):
+        recalculated_phenotype = (
+            e_0
+            + get_first_order_effect(e_1, genotype)
+            + get_second_order_effect(e_2, genotype)
+        )
+        assert np.allclose(recalculated_phenotype, phenotypes[idx], atol=1e-5)
