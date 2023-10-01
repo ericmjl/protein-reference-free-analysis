142190f39af3474c886310be5f8697faf54c3e90
Eric Ma <ericmajinglong@gmail.com>
2023-10-01 10:11:36 -0400

    refactor(genotype_generator): rename num_positions to num_sites
    - Changed the parameter name from num_positions to num_sites in the make_comprehensive_genotypes function for better clarity.
    - Also, rearranged the import statements according to PEP8 guidelines.

--- protein_reference_free_analysis/genotype_generator.py
+++ protein_reference_free_analysis/genotype_generator.py
@@ -1,19 +1,20 @@
 """Functions that generate a genotype matrix."""
 
-import jax.numpy as np
 from itertools import product
 
+import jax.numpy as np
 
-def make_comprehensive_genotypes(num_states: int, num_positions: int) -> np.ndarray:
+
+def make_comprehensive_genotypes(num_states: int, num_sites: int) -> np.ndarray:
     """Make a comprehensive genotype matrix.
 
     :param num_states: The number of genotype states desired.
-    :param num_positions: The number of genotype positions desired.
+    :param num_sites: The number of genotype positions desired.
     :return: A comprehensive genotype matrix of all possible genotypes.
     """
     genotypes_oh = np.eye(num_states, dtype=np.int8)
     genotypes = []
-    for genotype in product(range(num_states), repeat=num_positions):
+    for genotype in product(range(num_states), repeat=num_sites):
         genotype = np.array([genotypes_oh[position] for position in genotype])
         genotypes.append(genotype)
     genotypes = np.array(genotypes)
